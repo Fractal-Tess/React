fa44c0d292284d701aa8bfe08cd2817276408d40
Fractal-Tess <vgfractal@gmail.com>
2022-09-21 06:34:46 +0300

    added eslint-json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,11 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:json/recommended',
     'prettier'
   ],
   rules: {
-    'no-unused-vars': 'off',
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'warn',
