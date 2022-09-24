6e330d40ef5c31159528adb1d7983944ffe29589
Fractal-Tess <vgfractal@gmail.com>
2022-09-24 08:29:39 +0300

    fixed shadow not extended

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,27 +8,26 @@ const config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['roboto', ...defaultTheme.fontFamily.sans],
+        sans: ['roboto', ...defaultTheme.fontFamily.sans]
       },
-    },
-    boxShadow: {
-      'top-xl': '0 -20px 25px -5px rgba(0, 0,  0,  .1)',
-    },
+      boxShadow: {
+        'top-xl': '0 -20px 25px -5px rgba(0, 0,  0,  .1)'
+      }
+    }
   },
-
   daisyui: {
     themes: [
       {
         dark: {
-          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
+          ...require('daisyui/src/colors/themes')['[data-theme=dark]']
         },
         light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
-        },
-      },
-    ],
+          ...require('daisyui/src/colors/themes')['[data-theme=light]']
+        }
+      }
+    ]
   },
-  plugins: [daisyui],
+  plugins: [daisyui]
 };
 
 module.exports = config;
